56c39d7ee8519be216695cf5dcf2596f474ba134
Eldor4 <65150710+Eldor4@users.noreply.github.com>
2021-07-16 01:52:03 +0300

    add border-bottom in subnav

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,29 +1,20 @@
 const downnav = document.querySelector(".nav_sticky");
 const header = document.querySelector("header");
 const class_sticky = "sticky";
-const test = "test";
 const down_nav_top = downnav.offsetTop;
 let lastScroll = 0; 
 
-//STICKY HEDER (You can remove the comments and open the console)
-
 window.onscroll= () => {
   const currentScroll = window.pageYOffset; 
-    if (currentScroll > lastScroll
-      && window.pageYOffset > down_nav_top){
+    if (currentScroll > lastScroll && window.pageYOffset > down_nav_top){
       downnav.classList.add(class_sticky)
       header.classList.remove(class_sticky)
-      // console.log('lastScroll:',lastScroll)
     } else if (
-      currentScroll < lastScroll ||
-      window.pageYOffset < down_nav_top
-      ) {
+      currentScroll < lastScroll || window.pageYOffset < down_nav_top){
         downnav.classList.remove(class_sticky);
         header.classList.add(class_sticky);
-        // console.log('lastScroll:',lastScroll)
       }
   lastScroll = currentScroll;
-  // console.log("currentScroll:",currentScroll)
 };
 
 
